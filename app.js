7a207eb45600924cb63bc496f656ba4bd1012c71
Sameera Bachoti <sameera@Sameeras-Air.home>
2017-05-05 00:40:42 -0400

    add app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', function(){
+	console.log("Connected to database " + config.database);
+});
+
+
+mongoose.connection.on('error', (err) => {
+	console.log("Database error: " + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+const workouts = require('./routes/workouts');
+
+const port = 3000; 
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+app.use('/workouts', workouts);
+
+app.get('/', (req, res) => {
+	res.send('Invalid endpoint.');
+});
+
+app.listen(port, () => {
+	console.log('Server started on port' + port);
+}); 
