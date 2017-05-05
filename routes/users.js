896566cc6224859b5ccc0e460ce880fc123a8557
Sameera Bachoti <sameera@Sameeras-Air.home>
2017-05-05 00:41:55 -0400

    add routes/

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
+var config = require('../config/database');
+var User = require('../models/user');
+
+
+router.post('/register', (req, res, next) => {
+	var newUser = new User({
+		name: req.body.name, 
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password 
+	});
+
+	User.addUser(newUser, (err, user) => {
+		if(err){
+			res.json({success: false, msg: 'Failed to register user'});
+		}else{
+			res.json({success: true, msg: 'User registered'});
+		}
+	});
+});
+
+router.post('/authenticate', (req, res, next) => {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	User.getUserByUsername(username, (err, user) => {
+	
+		if(!user){
+			return res.json({success: false, msg: 'User not found'});
+		}
+
+		User.comparePassword(password, user.password, (err, isMatch) => {
+			
+			if(isMatch){
+				const token = jwt.sign(user, config.secret, {
+					expiresIn: 604800 // 1 week
+				});
+
+				res.json({
+					success: true, 
+					token: "JWT "+token, 
+					user: {
+						id: user._id, 
+						name: user.name, 
+						username: user.username, 
+						email: user.email
+					}
+				})
+			} else{
+				return res.json({success: false, msg: 'Wrong password'});
+			}
+		});
+
+	});
+
+	
+});
+
+
+module.exports = router;
---
+++ routes/workouts.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
+var config = require('../config/database');
+var Workout = require('../models/workout');
+MongoClient = require('mongodb').MongoClient;
+var {ObjectId} = require('mongodb');
+
+
+router.post('/add-workout', (req, res, next) => {
+	var workout = new Workout({
+		name: req.body.name, //what's submitted in the form
+		category: req.body.category,
+		length: req.body.length,
+		calories: req.body.calories, 
+		notes: req.body.notes,
+		user_id: req.body.user_id
+	});
+
+	Workout.addWorkout(workout, (err, workout) => {
+
+		if(err){
+			res.send({success: false, msg: 'Failed to add workout'});
+		}else{
+			res.send({success: true, msg: 'Workout added.'});
+		}
+	});
+});
+
+//Get all workouts
+router.get('/workouts', (req, res, next) => {
+	
+	MongoClient.connect("mongodb://localhost:27017/fitness-app", function (err, db) {
+    
+		var workouts = db.collection('workouts');
+
+		workouts.find({}, function(err, workout) {
+		    workout.toArray(function(err, data){
+		    	res.send(data);
+		    });
+		});
+                
+	});
+
+	
+});
+
+//Get single workout
+router.get('/workout/:id', (req, res, next) => {
+	
+	MongoClient.connect("mongodb://localhost:27017/fitness-app", function (err, db) {
+    
+		var workouts = db.collection('workouts');
+		workouts.findOne({_id: ObjectId(req.params.id)}, function(err, data) {
+		    res.send(data);
+		});
+                
+	});
+
+});
+
+//Update workout
+router.put('/workout/:id', (req, res, next) => {
+
+	var workout = req.body; 
+	var updatedWorkout = {};
+
+		updatedWorkout.name = workout.name;
+	
+
+		updatedWorkout.category = workout.category;
+	
+
+		updatedWorkout.length = workout.length;
+	
+		updatedWorkout.notes = workout.notes;
+	
+
+		MongoClient.connect("mongodb://localhost:27017/fitness-app", function (err, db) {
+    
+			var workouts = db.collection('workouts');
+			workouts.update({_id: ObjectId(req.params.id)},updatedWorkout,{}, function(err, data) {
+			    res.send(data);
+			});
+	                
+		});
+});
+
+//Delete Task 
+router.delete('/workout/:id', (req, res, next) => {
+	
+	MongoClient.connect("mongodb://localhost:27017/fitness-app", function (err, db) {
+    
+		var col = db.collection('workouts');
+		col.remove({_id: ObjectId(req.params.id)}, function(err, cursor) {
+		    res.send(cursor.id);
+		});
+                
+	});
+
+	
+});
+
+module.exports = router;
